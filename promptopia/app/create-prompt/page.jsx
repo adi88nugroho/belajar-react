e081a3f99b03cc5581faa8ee2d609a686bb1612d
adi88nugroho <adi88nugroho@gmail.com>
2024-05-17 13:48:10 +0700

    chore: Update NextAuth import and fix user creation logic in signin callback

--- promptopia/app/create-prompt/page.jsx
+++ promptopia/app/create-prompt/page.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { Router, useRouter } from "next/router";
+import Form from "@components/Form";
+import { set } from "mongoose";
 
 const CreatePrompt = () => {
-  return <div>CreatePrompt</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: "",
+    tag: "",
+  });
+
+  const CreatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch("/api/create-prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        Router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      CreatePrompt={CreatePrompt}
+    />
+  );
 };
 
 export default CreatePrompt;
